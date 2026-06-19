dc01d31f95afa7eb9dd8eb5e19ced5e1b526fa58
anushka-r-5 <anushka.ramrakhya@pockydoc.com>
2026-06-19 22:07:36 +0530

    integrate oauth using zoho api console

--- app/src/main/java/com/IC/zohotaskswidget/utils/Constants.kt
+++ app/src/main/java/com/IC/zohotaskswidget/utils/Constants.kt
@@ -1,15 +1,15 @@
 package com.IC.zohotaskswidget.utils
 
-object Constants {
+object `Constants.kt` {
 
-    const val CLIENT_ID = "YOUR_ZOHO_CLIENT_ID"
+    const val CLIENT_ID = "1000.S2DN6L9WPOKUUVM8GWLKERF2HSTD3E"
 
-    const val CLIENT_SECRET = "YOUR_ZOHO_CLIENT_SECRET"
+    const val CLIENT_SECRET = "91d34d66e9b68a85ddd41da58d4e6ea1ce6ea6633e"
 
     const val REDIRECT_URI = "com.ic.zohotaskswidget://oauth"
 
-    const val ACCOUNTS_BASE_URL =
-        "https://accounts.zoho.com"
-    const val API_BASE_URL =
-        "https://www.zohoapis.in/crm/v8/"
+    const val ACCOUNTS_BASE_URL = "https://accounts.zoho.com"
+
+    const val API_BASE_URL = "https://tasks.zoho.com/api/v1/"
+}
 }
